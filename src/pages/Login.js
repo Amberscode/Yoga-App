f5be93ec60e8340aba8b34ccc0ebc6ef2fcb6acc
amber <amber.nowakowski@gmail.com>
2022-06-28 15:20:40 +0200

    add navbar and setup routes

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+
+const Login = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Login Page</h1>;
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/SignUp.js
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+
+const SignUp = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Sign Up Page</h1>
+    </div>
+  );
+};
+
+export default SignUp;
