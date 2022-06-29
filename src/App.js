7ce368d9315e11ed531775ae12a2142567616f83
amber <amber.nowakowski@gmail.com>
2022-06-29 10:45:25 +0200

    create wrapper component for styles and navbar and move routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import "./App.css";
-import NavBar from "./components/NavBar";
+import { Route, Routes, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Schedule from "./pages/Schedule";
+import Contact from "./pages/Contact";
+import ClassDetail from "./pages/ClassDetail";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <h1>Yoga App</h1>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="about" element={<About />} />
+        <Route path="schedule" element={<Schedule />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="classdetail" element={<ClassDetail />} />
+      </Routes>
+    </Layout>
   );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,6 @@
-import NavBar from "../components/NavBar";
-
 const About = () => {
   return (
     <div>
-      <NavBar />
       <h1>About Page</h1>;
     </div>
   );
