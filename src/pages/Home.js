139695e18cea45da67d3e631a639a8e8bebb8e1d
Amber <amber@Ambers-MacBook-Air.local>
2022-07-04 13:25:37 +0200

    add homepage parallax scrolling styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,80 @@
-function Home() {
+import "../styles/Home.css";
+
+const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>;
+      <section className="parallax">
+        <div className="parallax-inner-1">
+          <h1>Welcome to Yoga</h1>
+        </div>
+      </section>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis
+        quibusdam, ex totam aliquam provident alias culpa, sit illo, eum
+        doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam
+        praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
+        nemo veniam maiores vitae deserunt cum ducimus.
+      </h4>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis
+        quibusdam, ex totam aliquam provident alias culpa, sit illo, eum
+        doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam
+        praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
+        nemo veniam maiores vitae deserunt cum ducimus.
+      </h4>
+      <section className="parallax-1">
+        <div className="parallax-inner">
+          <h3>Yoga quote</h3>
+        </div>
+      </section>
+
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis
+        quibusdam, ex totam aliquam provident alias culpa, sit illo, eum
+        doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam
+        praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
+        nemo veniam maiores vitae deserunt cum ducimus.
+      </h4>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis
+        quibusdam, ex totam aliquam provident alias culpa, sit illo, eum
+        doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam
+        praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
+        nemo veniam maiores vitae deserunt cum ducimus.
+      </h4>
+      <section className="parallax-2">
+        <div className="parallax-inner">
+          <h3>Another yoga quote</h3>
+        </div>
+      </section>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis
+        quibusdam, ex totam aliquam provident alias culpa, sit illo, eum
+        doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam
+        praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
+        nemo veniam maiores vitae deserunt cum ducimus.
+      </h4>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis
+        quibusdam, ex totam aliquam provident alias culpa, sit illo, eum
+        doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam
+        praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
+        nemo veniam maiores vitae deserunt cum ducimus.
+      </h4>
     </div>
   );
-}
+};
 
 export default Home;
