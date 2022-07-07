ec2f7729aebed04b26543e76ee3b5942177ca269
Amber <amber@Ambers-MacBook-Air.local>
2022-07-07 20:54:40 +0200

    adjust footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,29 +12,27 @@ const Footer = () => {
     <footer className="text-center text-lg-start ">
       <div className="container text-center text-md-start">
         <div className="row">
-          <div className="col-md-4 mx-auto mb-3">
-            <h6 className="text-uppercase fw-bold mb-4">
-              <i className="fas fa-gem me-3"></i>Yoga Studio
-            </h6>
-            <p>
+          <div className="col-md-4 mx-auto ">
+            <h6 className="text-uppercase fw-bold pb-2 pt-4">Yoga Studio</h6>
+            <p className="mb-0">
               <FontAwesomeIcon icon={faHouse} /> Av Paseo de la Marina 121,
               Marina Vallarta, Puerto Vallarta, Mexico
             </p>
           </div>
 
-          <div className="col-md-4 mx-auto mb-md-0 mb-3">
-            <h6 className="text-uppercase fw-bold mb-3">Contact</h6>
-            <p>
+          <div className="col-md-4 mx-auto mb-md-0 ">
+            <h6 className="text-uppercase fw-bold pb-2 pt-4">Contact</h6>
+            <p className="mb-0">
               <FontAwesomeIcon icon={faEnvelope} /> yogastudio@contact.com
             </p>
-            <p>
+            <p className="mb-0 ">
               <FontAwesomeIcon icon={faPhone} /> (322) 555-5555
             </p>
           </div>
         </div>
       </div>
-      <div className="text-center p-2">
-        <FontAwesomeIcon icon={faSpa} className="fa-xl" />
+      <div className="text-center pb-2 ">
+        <FontAwesomeIcon icon={faSpa} className="fa-2xl" />
       </div>
     </footer>
   );
