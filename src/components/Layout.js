d9eba6e9aa0b0c7a956f3687db4ebc34189baa0c
Amber <amber@Ambers-MacBook-Air.local>
2022-07-07 18:12:25 +0200

    add footer component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import classes from "./Layout.module.css";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 const Layout = (props) => {
   return (
     <Fragment>
       <NavBar />
       <main className={classes.main}>{props.children}</main>
+      <Footer />
     </Fragment>
   );
 };
