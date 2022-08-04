ebb45f56adfce79266d90b33d4c4d7ef2bd4e628
amber <amber.nowakowski@gmail.com>
2022-08-03 18:01:18 -0600

    add another home page image and section and adjust colors and styles and add quotes

--- src/components/PriceGrid.js
+++ src/components/PriceGrid.js
@@ -4,28 +4,28 @@ const PriceGrid = () => {
   return (
     <div class="row justify-content-md-center price-grid mt-1 mb-5">
       <div class="col-lg-2 price-grid-pink mx-3 my-2">
-        <h2 class="display-4">$25</h2>
+        <h2 class="price">$25</h2>
         <p class="price-description">SINGLE CLASS DROP-IN</p>
       </div>
       <div class="col-lg-2 price-grid-blue mx-3 my-2">
-        <h2 class="display-4">$95</h2>
+        <h2 class="price">$95</h2>
         <p class="price-description">5 CLASS DROP-IN</p>
       </div>
       <div class="col-lg-2 price-grid-pink mx-3 my-2">
-        <h2 class="display-4">$190</h2>
+        <h2 class="price">$190</h2>
         <p class="price-description">10 CLASS DROP-IN</p>
       </div>
       <div class="w-100"></div>
       <div class="col-lg-2 price-grid-blue mx-3 my-2">
-        <h2 class="display-4">$370</h2>
+        <h2 class="price">$370</h2>
         <p class="price-description">20 CLASS DROP-IN</p>
       </div>
       <div class="col-lg-2 price-grid-pink mx-3 my-2">
-        <h2 class="display-4">$210</h2>
+        <h2 class="price">$210</h2>
         <p class="price-description">1 MONTH UNLIMITED</p>
       </div>
       <div class="col-lg-2 price-grid-blue mx-3 my-2">
-        <h2 class="display-4">$1800</h2>
+        <h2 class="price">$1800</h2>
         <p class="price-description">1 YEAR UNLIMITED</p>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,15 @@ const Home = () => {
       </h4>
       <section className="parallax-1">
         <div className="parallax-inner">
-          <h3>Yoga quote</h3>
+          <h3>
+            <em>
+              “Yoga does not just change the way we see things, it transforms
+              the person who sees.”
+            </em>
+          </h3>
+          <h3 className="float-end">
+            <em>― B.K.S Iyengar</em>
+          </h3>
         </div>
       </section>
 
@@ -51,13 +59,58 @@ const Home = () => {
         praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
         nemo veniam maiores vitae deserunt cum ducimus.
       </h4>
+
       <section className="parallax-2">
         <div className="parallax-inner">
-          <h3>Another yoga quote</h3>
+          <h3>
+            <em>
+              “True yoga is not about the shape of your body, but the shape of
+              your life. Yoga is not to be performed; yoga is to be lived. Yoga
+              doesn’t care about what you have been; yoga cares about the person
+              you are becoming. Yoga is designed for a vast and profound
+              purpose, and for it to be truly called yoga, its essence must be
+              embodied.”
+            </em>
+          </h3>
+          <h3 className="float-end">
+            <em>— Aadil Palkhivala</em>
+          </h3>
         </div>
       </section>
-      <h2 className="display-4 price-header">Pricing</h2>
+
+      <h2 className="price-header">Pricing</h2>
       <PriceGrid />
+
+      <section className="parallax-3">
+        <div className="parallax-inner">
+          <h3>
+            <em>
+              “Yoga is the journey of the self, through the self, to the self.”
+            </em>
+          </h3>
+          <h3 className="float-end">
+            <em>― The Bhagavad Gita</em>
+          </h3>
+        </div>
+      </section>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis
+        quibusdam, ex totam aliquam provident alias culpa, sit illo, eum
+        doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam
+        praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
+        nemo veniam maiores vitae deserunt cum ducimus.
+      </h4>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        minima fuga debitis quasi eius aliquid sapiente? Cumque blanditiis
+        quibusdam, ex totam aliquam provident alias culpa, sit illo, eum
+        doloribus doloremque. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Excepturi reprehenderit voluptatum aperiam pariatur numquam
+        praesentium recusandae, ipsa at iusto eveniet, distinctio sunt dolore
+        nemo veniam maiores vitae deserunt cum ducimus.
+      </h4>
     </div>
   );
 };
